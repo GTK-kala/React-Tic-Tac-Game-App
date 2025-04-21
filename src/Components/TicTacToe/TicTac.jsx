b333bb7a5b4ef59cba1223f31e1d3605d55b133d
GTK-kala <gtkkala1110@gmail.com>
2025-04-21 22:04:37 +0300

    fourth commit

--- src/Components/TicTacToe/TicTac.jsx
+++ src/Components/TicTacToe/TicTac.jsx
@@ -1,9 +1,50 @@
+// import circle_icon from '../Assets/circle.png'
+// import cross_icon from '../Assets/cross.png'
 import  './TicTac.css'
+
 
 const TicTac = () => {
   return (
     <>
-      
+      <div className="container">
+         <h1 className='title'>Tic Tac Toe Game In <span>  React</span></h1>
+          <div className="board">
+             <div className="row1">
+               <div className="boxes">
+                 
+               </div>
+               <div className="boxes">
+
+               </div>
+               <div className="boxes">
+
+               </div>
+             </div>
+             <div className="row2">
+               <div className="boxes">
+                 
+               </div>
+               <div className="boxes">
+
+               </div>
+               <div className="boxes">
+                
+               </div>
+             </div>
+             <div className="row3">
+               <div className="boxes">
+                 
+               </div>
+               <div className="boxes">
+
+               </div>
+               <div className="boxes">
+                
+               </div>
+             </div>
+          </div>
+          <button className="reset">Reset</button>
+      </div>
     </>
   )
 }
